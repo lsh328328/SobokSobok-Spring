47eaddbc148051c13883275fedbd574f8260038f
Crayon <crayon@iseungheon-ui-MacBookPro.local>
2022-12-22 16:45:47 +0900

    Setting: 서버 세팅

---
+++ src/main/java/com/deploy/SobokSobok/SobokSobokApplication.java
@@ -0,0 +1,13 @@
+package com.deploy.SobokSobok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SobokSobokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SobokSobokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deploy/SobokSobok/SobokSobokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.SobokSobok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SobokSobokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
