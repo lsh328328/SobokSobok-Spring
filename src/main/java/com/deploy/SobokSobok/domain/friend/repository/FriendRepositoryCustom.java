6bec1e583862006e7fbeb991fc1bf39ec3c12958
Lee SeungHeon <lsh328328@naver.com>
2023-01-23 19:39:13 +0900

    Setting [#6] Friend Entity 생성

---
+++ src/main/java/com/deploy/SobokSobok/domain/friend/repository/FriendRepositoryCustom.java
@@ -0,0 +1,4 @@
+package com.deploy.SobokSobok.domain.friend.repository;
+
+public interface FriendRepositoryCustom {
+}
---
+++ src/main/java/com/deploy/SobokSobok/domain/friend/repository/FriendRepositoryCustomImpl.java
@@ -0,0 +1,4 @@
+package com.deploy.SobokSobok.domain.friend.repository;
+
+public class FriendRepositoryCustomImpl implements FriendRepositoryCustom {
+}
